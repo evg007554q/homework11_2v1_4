1f1be11ad3ea44d54bfd2f843e5697fee7c0b174
Evg <evg007554@yandex.ru>
2023-04-27 21:40:39 +0300

    3.3

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def capital_letters(value):
+    "Добавьте docstring к функции из предыдущего задания (прямо в ветке develop). Этим вы сымитируете проделанную работу другого разработчика над этим же модулем."
     return value.upper()
+
+def title_str(value):
+    return value.title()

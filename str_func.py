a246786044f1aea413c0a087756918a7c8616275
evg007554q <131668392+evg007554q@users.noreply.github.com>
2023-04-27 22:09:20 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,7 @@
 def capital_letters(value):
-    "Добавьте docstring к функции из предыдущего задания (прямо в ветке develop). test 1"
+    "Добавьте docstring к функции из предыдущего задания (прямо в ветке develop). test 2"
     return value.upper()
 
 def title_str(value):
-    "Добавьте docstring к функции из предыдущего задания (прямо в ветке develop). test 1"
+    "Добавьте docstring к функции из предыдущего задания (прямо в ветке develop). test 2"
     return value.title()
